daa0fcdadb8bd157f8ebb3c5bdcdc44f1f380bac
THELUX1 <122177610+THELUX1@users.noreply.github.com>
2025-08-19 16:58:29 -0300

    Add files via upload

---
+++ assets/js/series-links.js
@@ -0,0 +1,26 @@
+const seriesLinks = {    
+    "119051": {
+        title: "Merlina",
+        seasons: {
+            "1": {
+                episodes: {
+                    "1": {
+                        title: "rty",
+                        sources: [
+                            { quality: "HD", url: "https://m5qqjwpatpzb.acek-cdn.com/hls2/01/05952/ke97tp6kmnwv_,l,n,h,.urlset/index-f2-v1-a1.m3u8?t=iNjC90spzZHAyZukXx3PskH47ybkKLNdW4D8gDkbA9s&s=1755591232&e=129600&f=29762396&srv=5TICbOQKpSaA&i=0.4&sp=500&p1=5TICbOQKpSaA&p2=5TICbOQKpSaA&asn=264738" }
+                        ],
+                        subtitles: [
+                            { lang: "Español", url: "https://example.com/series/got/s1/e1/subs/es.vtt" }
+                        ]
+                    }
+                    // ... más episodios
+                }
+            }
+            // ... más temporadas
+        }
+    }
+    // ... más series
+};
+
+// Exportación correcta
+export { seriesLinks };
